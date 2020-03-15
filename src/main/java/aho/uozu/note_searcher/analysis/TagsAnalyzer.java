78e484daa5f414ec96957df3c49ab7005155b74a
Warwick Stone <uozu.aho@gmail.com>
2020-03-15 22:45:34 +1100

    d'oooh I'm nearly there I can taste it
    so tired

--- src/main/java/aho/uozu/note_searcher/analysis/TagsAnalyzer.java
+++ src/main/java/aho/uozu/note_searcher/analysis/TagsAnalyzer.java
@@ -10,22 +10,21 @@ import org.apache.lucene.analysis.util.CharTokenizer;
 import java.io.IOException;
 import java.io.StringReader;
 
-public class TaggingAnalyzer extends Analyzer {
+public class TagsAnalyzer extends Analyzer {
 
-    public TaggingAnalyzer() {}
+    public TagsAnalyzer() {}
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-//        return new TokenStreamComponents(new StandardTokenizer());
-        final Tokenizer tokenizer = new TaggingTokenizer();
-        final TokenStream stream = new TaggingTokenFilter(tokenizer);
+        final Tokenizer tokenizer = new TagsTokenizer();
+        TokenStream stream = new TagsTokenFilter(tokenizer);
         return new TokenStreamComponents(tokenizer, stream);
     }
 
     public static void main(String[] args) throws IOException {
         final String text = "Demo-text, Including \"tags\" like #wow. Not a tag: #";
 
-        var analyzer = new TaggingAnalyzer();
+        var analyzer = new TagsAnalyzer();
         var stream = analyzer.tokenStream("field", new StringReader(text));
 
         // get the CharTermAttribute from the TokenStream...what???
@@ -44,18 +43,18 @@ public class TaggingAnalyzer extends Analyzer {
         }
     }
 
-    private class TaggingTokenizer extends CharTokenizer {
+    private static class TagsTokenizer extends CharTokenizer {
         @Override
         protected boolean isTokenChar(int c) {
             return Character.isLetterOrDigit(c) || c == '#';
         }
     }
 
-    private class TaggingTokenFilter extends TokenFilter {
+    /** A 'filtering filter' that only returns tag tokens */
+    private static class TagsTokenFilter extends TokenFilter {
         private CharTermAttribute _termAtt;
 
-        public TaggingTokenFilter(TokenStream input)
-        {
+        public TagsTokenFilter(TokenStream input) {
             super(input);
             this._termAtt = this.addAttribute(CharTermAttribute.class);
         }
@@ -63,32 +62,21 @@ public class TaggingAnalyzer extends Analyzer {
         @Override
         public boolean incrementToken() throws IOException {
             while (this.input.incrementToken()) {
-                char[] term = _termAtt.buffer();
-                if (containsHash(term)) {
-                    // contains a hash but isn't a tag: ignore
-                    if (!isATag(_termAtt)) continue;
-
-                    // it's a tag. Remove the hash from the buffer.
-                    _termAtt.copyBuffer(_termAtt.buffer(), 1, _termAtt.length() - 1);
-                }
+                if (!isATag(_termAtt)) continue;
+                removeHashPrefix();
                 return true;
             }
             return false;
+        }
+
+        private void removeHashPrefix() {
+            _termAtt.copyBuffer(_termAtt.buffer(), 1, _termAtt.length() - 1);
         }
 
         private boolean isATag(CharTermAttribute term) {
             if (term.length() < 2) return false;
             char[] chars = term.buffer();
             return chars[0] == '#' && Character.isLetterOrDigit(chars[1]);
-        }
-
-        private boolean containsHash(char[] chars) {
-            for (char c : chars) {
-                if (c == '#') {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
--- src/test/java/aho/uozu/note_searcher/EnglishAnalyzerSearchTests.java
+++ src/test/java/aho/uozu/note_searcher/EnglishAnalyzerSearchTests.java
@@ -5,14 +5,13 @@ import org.apache.lucene.index.memory.MemoryIndex;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class EnglishAnalyzerTests {
+public class EnglishAnalyzerSearchTests {
     private EnglishAnalyzer _analyzer;
     private MemoryIndex _index;
 
@@ -24,33 +23,30 @@ public class EnglishAnalyzerTests {
         _index.addField("author", "Tales of James", _analyzer);
     }
 
-    @Nested
-    class Search {
-        @Test
-        public void isCaseInsensitive() throws ParseException {
-            float score = search("Salmons Alaska Manuals");
-            float lowercaseScore = search("salmons alaska manuals");
+    @Test
+    public void isCaseInsensitive() throws ParseException {
+        float score = search("Salmons Alaska Manuals");
+        float lowercaseScore = search("salmons alaska manuals");
 
-            assertThat(score, equalTo(lowercaseScore));
-        }
+        assertThat(score, equalTo(lowercaseScore));
+    }
 
-        @Test
-        public void ignoresPlurals() throws ParseException {
-            float score = search("Salmons Alaska Manuals");
-            float withoutPluralsScore = search("salmon alaska manual");
+    @Test
+    public void ignoresPlurals() throws ParseException {
+        float score = search("Salmons Alaska Manuals");
+        float withoutPluralsScore = search("salmon alaska manual");
 
-            assertThat(score, equalTo(withoutPluralsScore));
-        }
+        assertThat(score, equalTo(withoutPluralsScore));
+    }
 
-        @Test
-        public void complexExample() throws ParseException {
-            float score = search("+author:james +salmon~ +fish* manual~");
-        }
+    @Test
+    public void complexExample() throws ParseException {
+        float score = search("+author:james +salmon~ +fish* manual~");
+    }
 
-        @Test
-        public void throwsParseException_ForBadQuery() {
-            assertThrows(ParseException.class, () -> search("meat pie! yo +++ "));
-        }
+    @Test
+    public void throwsParseException_ForBadQuery() {
+        assertThrows(ParseException.class, () -> search("meat pie! yo +++ "));
     }
 
     private float search(String query) throws ParseException {
