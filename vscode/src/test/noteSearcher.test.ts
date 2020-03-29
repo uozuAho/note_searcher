94d009807dc8d23a7402aeffc969980d2247b79e
Warwick Stone <uozu.aho@gmail.com>
2020-03-29 23:09:33 +1100

    more test, why no error?

--- vscode/src/test/noteSearcher.test.ts
+++ vscode/src/test/noteSearcher.test.ts
@@ -9,21 +9,61 @@ describe('NoteSearcher', () => {
   let noteSearcher: NoteSearcher;
 
   const user_input_on_search = (input: string) => {
-    ui.setup(u => u.promptForSearch(tmoq.It.isAnyString())).returns(
-      () => Promise.resolve(input));
+    ui.setup(u =>
+      u.promptForSearch(tmoq.It.isAnyString()))
+      .returns(
+        () => Promise.resolve(input)
+      );
   };
 
-  beforeEach(() => {
-    searcher = tmoq.Mock.ofType<Searcher>();
-    ui = tmoq.Mock.ofType<NoteSearcherUi>();
-    noteSearcher = new NoteSearcher(ui.object, searcher.object);
-  });
+  const searcher_returns = (results: string[]) => {
+    searcher.setup(s =>
+      s.search(tmoq.It.isAnyString()))
+      .returns(
+        () => Promise.resolve(results)
+      );
+  };
 
-  it('search passes input to searcher', async () => {
-    user_input_on_search('search phrase');
+  describe('search', () => {
+    beforeEach(() => {
+      searcher = tmoq.Mock.ofType<Searcher>();
+      ui = tmoq.Mock.ofType<NoteSearcherUi>();
+      noteSearcher = new NoteSearcher(ui.object, searcher.object);
+    });
 
-    await noteSearcher.search();
+    it('passes input to searcher', async () => {
+      user_input_on_search('search phrase');
 
-    searcher.verify(s => s.search('search phrase'), tmoq.Times.once());
+      await noteSearcher.search();
+
+      searcher.verify(s => s.search('search phrase'), tmoq.Times.once());
+    });
+
+    it('does nothing when input is empty', async () => {
+      user_input_on_search('');
+
+      await noteSearcher.search();
+
+      searcher.verify(s => s.search(tmoq.It.isAnyString()), tmoq.Times.never());
+    });
+
+    it('shows search results', async () => {
+      user_input_on_search('search phrase');
+      searcher_returns(['a', 'b', 'c']);
+
+      await noteSearcher.search();
+
+      ui.verify(u => u.showSearchResults(['a', 'b', 'c']), tmoq.Times.once());
+    });
+
+    it('shows error when search throws', async () => {
+      const error = new Error('boom');
+      searcher.setup(s => s.search(tmoq.It.isAnyString())).throws(error);
+
+      await noteSearcher.search();
+
+      ui.verify(u => u.showSearchResults(tmoq.It.isAny()), tmoq.Times.never());
+      ui.verify(u => u.showError(tmoq.It.isAny()), tmoq.Times.once());
+    });
   });
 });
