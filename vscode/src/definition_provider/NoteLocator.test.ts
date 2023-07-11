ff9db1661fb2cbb015eddcea087dbd2dd7959df7
Warwick Stone <warwick_stone@fastmail.com>
2023-07-11 15:47:04 +1000

    rename test

--- vscode/src/definition_provider/NoteLocator.test.ts
+++ vscode/src/definition_provider/NoteLocator.test.ts
@@ -1,7 +1,7 @@
 import { NoteLocator } from './NoteLocator';
 import { InMemoryLinkIndex } from "../index/InMemoryLinkIndex";
 
-describe('WikiLinkDefinitionProvider: NoteLocator', () => {
+describe('NoteLocator', () => {
   let index: InMemoryLinkIndex;
   let noteLocator: NoteLocator;
   const note1 = process.platform === 'win32' ? 'C:\\a\\note1.md' : '/a/note1.md';
@@ -15,23 +15,23 @@ describe('WikiLinkDefinitionProvider: NoteLocator', () => {
     noteLocator = new NoteLocator(index);
   });
 
-  it('returns the note path if the note exists', async () => {
-    const result = await noteLocator.locateNote('note1');
+  it('returns the note path if the note exists', () => {
+    const result = noteLocator.locateNote('note1');
     expect(result).toEqual([note1]);
   });
 
-  it('returns an empty array if the note does not exist', async () => {
-    const result = await noteLocator.locateNote('note3');
+  it('returns an empty array if the note does not exist', () => {
+    const result = noteLocator.locateNote('note3');
     expect(result).toEqual([]);
   });
 
-  it('returns a single note path if the given name is a substring of a single note', async () => {
-    const result = await noteLocator.locateNote('note');
+  it('returns a single note path if the given name is a substring of a single note', () => {
+    const result = noteLocator.locateNote('note');
     expect(result).toEqual([note1, note2]);
   });
 
-  it('returns multiple results if the given name is a substring of multiple notes', async () => {
-    const result = await noteLocator.locateNote('note');
+  it('returns multiple results if the given name is a substring of multiple notes', () => {
+    const result = noteLocator.locateNote('note');
     expect(result).toEqual([note1, note2]);
   });
 });
