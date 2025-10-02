cf7abe2130c042c2083bcd4bafb17adf0747c7e8
me <me@me.com>
2025-10-02 14:32:25 +1000

    pass tests

---
+++ vscode/src/text_processing/wikilinkUpdater.test.ts
@@ -0,0 +1,16 @@
+import { updateLinks } from "./wikilinkUpdater";
+
+describe('wikilink text updater', () => {
+  it.each([
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[cat]]", "[[dog]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[ cat ]]", "[[ dog ]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "cat", "cat"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[cats]]", "[[cats]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my note about cats | cat]]", "[[my note about cats | dog]]"]
+    // todo: test multiline
+  ])('oldPath: %s, newPath: %s, oldText: %s, expectedText: %s',
+    (oldPath, newPath, noteText, expectedText) => {
+      const newText = updateLinks(oldPath, newPath, noteText);
+      expect(newText).toBe(expectedText);
+    });
+});
