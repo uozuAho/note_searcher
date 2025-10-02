06bfac62b357756b4cfd0a69cd0ad158bf3d9577
me <me@me.com>
2025-10-02 14:35:15 +1000

    test multiline

--- vscode/src/text_processing/wikilinkUpdater.test.ts
+++ vscode/src/text_processing/wikilinkUpdater.test.ts
@@ -6,8 +6,14 @@ describe('wikilink text updater', () => {
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[ cat ]]", "[[ dog ]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "cat", "cat"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[cats]]", "[[cats]]"],
-    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my note about cats | cat]]", "[[my note about cats | dog]]"]
-    // todo: test multiline
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my note about cats | cat]]", "[[my note about cats | dog]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt",
+      `Multiline note [[about cats | cat]]
+      hey ho [[cats]]
+      [[cat]]`,
+      `Multiline note [[about cats | dog]]
+      hey ho [[cats]]
+      [[dog]]`]
   ])('oldPath: %s, newPath: %s, oldText: %s, expectedText: %s',
     (oldPath, newPath, noteText, expectedText) => {
       const newText = updateLinks(oldPath, newPath, noteText);
