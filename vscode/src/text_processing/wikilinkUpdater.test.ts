a911b792bc86591270753b5d9f87eb9b246a42e6
me <me@me.com>
2025-10-03 14:09:51 +1000

    more tests, some break

--- vscode/src/text_processing/wikilinkUpdater.test.ts
+++ vscode/src/text_processing/wikilinkUpdater.test.ts
@@ -2,12 +2,22 @@ import { updateLinks } from "./wikilinkUpdater";
 
 describe('wikilink text updater', () => {
   it.each([
+    ["/a/b/cat.txt", "/a/b/dog.txt", "", ""],
+    ["/a/b/cat.txt", "/a/b/cat.txt", "[[cat]]", "[[cat]]"],
+    ["/a/b/cat.txt", "/a/b/CAT.txt", "[[cat]]", "[[CAT]]"],
+    ["/a/b/cat.txt", "/a/b/cat.txt", "[[my asdf |cat]]", "[[my asdf |cat]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[cat]]", "[[dog]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[cat|and|cat]]", "[[cat|and|cat]]"], // not a valid link - max one pipe allowed
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[dir/cat]]", "[[dir/cat]]"], // not a valid link - must be filename only
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[cat cat]]", "[[cat cat]]"], // not a valid link - can't have whitespace
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my | cat cat]]", "[[my | cat cat]]"], // not a valid link - can't have whitespace
+    ["/a/b/a.b.txt", "/a/b/a_b.txt", "[[a.b]]", "[[a_b]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[ cat ]]", "[[ dog ]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "cat", "cat"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "unrelated", "unrelated"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[cats]]", "[[cats]]"],
-    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my note about cats | cat]]", "[[my note about cats | dog]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my cat | cat]]", "[[my cat | dog]]"],
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my cats | cat]]", "[[my cats | dog]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[my note about cats|cat]]", "[[my note about cats|dog]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[unrelated | note]]", "[[unrelated | note]]"],
     ["/a/b/cat.txt", "/a/b/dog.txt", "[[unrelated | cats]]", "[[unrelated | cats]]"],
@@ -30,6 +40,9 @@ describe('wikilink text updater', () => {
       should ]] not be interpreted as a link.`,
       `Hey ho multiline, sometimes I like to type [[ but this
       should ]] not be interpreted as a link.`],
+
+    // dunno why you'd do this, but replacing this text seems harmless
+    ["/a/b/cat.txt", "/a/b/dog.txt", "[[my [[cat]] yeah]]", "[[my [[dog]] yeah]]"],
   ])('oldPath: %s, newPath: %s, oldText: %s, expectedText: %s',
     (oldPath, newPath, noteText, expectedText) => {
       const newText = updateLinks(oldPath, newPath, noteText);
